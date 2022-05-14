23c7fac0a20d3993a843bcdc08a7e7ed97d4c0dd
Linventif <linventif@gmail.com>
2022-05-14 15:27:03 +0200

    2022 05 14

--- py-workshop-stats/Py-Workshop-DL.py
+++ py-workshop-stats/Py-Workshop-DL.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 
 import os
 
-workshop = ["-- GMOD Py Workshop-Downloader CREATOR - - Created by Linventif --"]
+workshop = []
+traiter = 0
+
 #Url de la collection a workshop dl
-url = "https://steamcommunity.com/sharedfiles/filedetails/?id=727825750"
+url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2717270825"
 
 html_code = urlopen(url).read().decode("utf-8")
 
@@ -16,23 +18,33 @@ prod = soup.find("div", class_="collectionChildren")
 addons = prod.findAll("div", class_="collectionItemDetails")
 
 
+
 for addon in addons:
+    print(f" - - - Nombre d'addons {len(addons)} - - - {traiter} / {len(addons)} - - - ")
+    traiter += 1
+
     title_addon = addon.find("a", href_="")
+    url = title_addon.get("href")
 
-    workshop.append(f'("{title_addon.get("href")}") -- {title_addon.text}')
+    html_code = urlopen(url).read().decode("utf-8")
+    soup = BeautifulSoup(html_code, "lxml")
+    prod = soup.find("div", class_="detailsStatRight")
+
+    workshop.append(f' {prod} -- {title_addon.get("href")} -- {title_addon.text}')
 
 
-workshop = "\nresource.AddWorkshop".join([str(elem) for elem in workshop])
+
+workshop = "\n".join([str(elem) for elem in workshop])
 workshop = workshop.replace("https://steamcommunity.com/sharedfiles/filedetails/?id=", "")
 
-
 try:
-    os.remove("workshop.lua")
-    open("workshop.lua", "x")
-    with open("workshop.lua", "w") as fichier:
+    os.remove("workshop.txt")
+    open("workshop.txt", "x")
+    with open("workshop.txt", "w") as fichier:
         fichier.write(workshop)
 
+
 except IOError:
-    open("workshop.lua", "x")
-    with open("workshop.lua", "w") as fichier:
+    open("workshop.txt", "x")
+    with open("workshop.txt", "w") as fichier:
         fichier.write(workshop)
