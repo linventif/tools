276039caf5cbc1f393fbde16c36d1ae86d53cb59
Linventif <linventif@gmail.com>
2022-05-14 17:27:30 +0200

    2022 05 14

--- py-workshop-stats/py-workshop-stats.py
+++ py-workshop-stats/py-workshop-stats.py
@@ -1,13 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
 import os
 
 workshop = []
 traiter = 0
+taille = 0
+taille_total = 0.0
 
-#Url de la collection a workshop dl
-url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2767798486"
+url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2397004463"
 
 html_code = urlopen(url).read().decode("utf-8")
 
@@ -17,34 +17,40 @@ prod = soup.find("div", class_="collectionChildren")
 
 addons = prod.findAll("div", class_="collectionItemDetails")
 
-
-
 for addon in addons:
-    print(f" - - - Nombre d'addons {len(addons)} - - - {traiter} / {len(addons)} - - - {round(traiter/len(addons)*100,2)}% - - - ")
+    print(f" - - - Nombre d'addons {len(addons)} - - - {taille_total}MB - - - {traiter} / {len(addons)} - - - {round(traiter / len(addons) * 100, 2)}% - - - ")
     traiter += 1
 
     title_addon = addon.find("a", href_="")
+
     url = title_addon.get("href")
 
     html_code = urlopen(url).read().decode("utf-8")
     soup = BeautifulSoup(html_code, "lxml")
     taille = soup.find("div", class_="detailsStatRight")
+    taille = taille.text
 
-    workshop.append(f' {taille.text} -- {title_addon.get("href")} -- {title_addon.text}')
+    workshop.append(f' {taille} -- {title_addon.get("href")} -- {title_addon.text}')
 
+    tt = taille.replace(" MB", "")
+    taille_total = taille_total + float(tt)
+    taille_total = round(taille_total,2)
 
+print(f" - - - Nombre d'addons {len(addons)} - - - {taille_total}MB - - - {traiter} / {len(addons)} - - - {round(traiter/len(addons)*100,2)}% - - - ")
 
+workshop.append(f" - - - Nombre d'addons {len(addons)} - - - {taille_total}MB - - - {traiter} / {len(addons)} - - - {round(traiter / len(addons) * 100, 2)}% - - - ")
 workshop = "\n".join([str(elem) for elem in workshop])
 workshop = workshop.replace("https://steamcommunity.com/sharedfiles/filedetails/?id=", "")
 
 try:
     os.remove("workshop.txt")
     open("workshop.txt", "x")
-    with open("workshop.txt", "w") as fichier:
+    with open("workshop.txt", "w", encoding='utf8') as fichier:
         fichier.write(workshop)
 
 
 except IOError:
     open("workshop.txt", "x")
-    with open("workshop.txt", "w") as fichier:
+    with open("workshop.txt", "w", encoding='utf8') as fichier:
         fichier.write(workshop)
+
